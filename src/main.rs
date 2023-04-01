e9da102bea85e12d64264fa8cfc6394ddd9e9153
WaterFace <kevinvisserweckworth@gmail.com>
2023-04-01 17:52:09 -0400

    Added a basic procedural texture for background stars

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::render_resource::Extent3d};
+mod starfield_image;
 
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    commands.spawn(Camera2dBundle {
+        ..Default::default()
+    });
+
+    let star_field = images.add(starfield_image::BasicStarField::default().build(Extent3d {
+        width: 1024,
+        height: 1024,
+        ..Default::default()
+    }));
+
+    commands.spawn(SpriteBundle {
+        texture: star_field,
+        transform: Transform::from_scale(Vec3::new(2.5, 2.5, 1.0)),
+        ..Default::default()
+    });
+}
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_startup_system(setup)
+        .run();
 }
