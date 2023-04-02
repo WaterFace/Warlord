3e6d8b9fb03159e05da3f24155409d93ad60ed2a
WaterFace <kevinvisserweckworth@gmail.com>
2023-04-01 23:38:26 -0400

    improved the camera's following capabilities

--- src/camera.rs
+++ src/camera.rs
@@ -8,6 +8,7 @@ pub struct SmoothFollow {
     pub target: Option<Entity>,
     pub offset: Vec3,
     pub focus_radius: f32,
+    pub focus_centering: f32,
 }
 
 impl Default for SmoothFollow {
@@ -16,6 +17,7 @@ impl Default for SmoothFollow {
             target: None,
             offset: Vec3::new(0.0, 0.0, 10.0),
             focus_radius: 100.0,
+            focus_centering: 0.5,
         }
     }
 }
@@ -49,13 +51,19 @@ fn follow_target(
         // Now `focus` holds the point we want the camera to follow, however we got it
         if smooth_follow.focus_radius > 0.0 {
             let dist = Vec3::distance(focus + smooth_follow.offset, transform.translation);
+            let mut t = 1.0;
+            if dist > 0.01 && smooth_follow.focus_centering > 0.0 {
+                t = f32::powf(1.0 - smooth_follow.focus_centering, time.delta_seconds());
+            }
             if dist > smooth_follow.focus_radius {
                 transform.translation = Vec3::lerp(
                     focus,
                     transform.translation,
                     smooth_follow.focus_radius / dist,
                 );
+                t = f32::min(t, smooth_follow.focus_radius / dist);
             }
+            transform.translation = Vec3::lerp(focus, transform.translation, t);
         } else {
             transform.translation = focus;
         }
